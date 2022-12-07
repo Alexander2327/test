8c78c0e61c9d6e05bd306d162e3fff2b4a27f1b7
Alexander2327 <danilov.aa1@mail.ru>
2022-12-07 11:47:36 +0300

    add tests

---
+++ src/__tests__/indexTest.js
@@ -0,0 +1,15 @@
+import checkHealth from '../index';
+
+const dataList = [
+  [{ name: 'Маг', health: 90 }, 'healthy'],
+  [{ name: 'Маг', health: 40 }, 'wounded'],
+  [{ name: 'Маг', health: 10 }, 'critical'],
+  [{ name: 'Маг', health: 'something' }, 0],
+];
+
+const handler = test.each(dataList);
+
+handler('test function with health %s', (obj, expected) => {
+  const result = checkHealth(obj);
+  expect(result).toBe(expected);
+});
---
+++ src/index.js
@@ -0,0 +1,12 @@
+export default function checkHealth(obj) {
+  if (obj.health > 50) {
+    return 'healthy';
+  }
+  if (obj.health >= 15 && obj.health <= 50) {
+    return 'wounded';
+  }
+  if (obj.health < 15) {
+    return 'critical';
+  }
+  return 0;
+}
